cb7cbcaf16aff35c33b2bb25de55938076ec78f1
Adrian Stoll <adrs@umich.edu>
2018-07-12 01:02:18 -0400

    remove O(n) memory requirement for jump game [BFS]

--- 45.jump.game.ii.cpp
+++ 45.jump.game.ii.cpp
@@ -44,13 +44,6 @@ int jump(vector<int>& nums) {
 		return 1;
 	}
 
-	// O(n)
-	vector<int> numJumps(nums.size(), INT_MAX);
-	numJumps[0] = 0;
-
-	// Mark endpoint of range from first num
-	numJumps[nums[0]] = 1;
-
 	// Index of marker farthest to right with numJumps = nj
 	int furthestMarker = nums[0];
 	// What furthestMarker will be after next jump
@@ -58,19 +51,7 @@ int jump(vector<int>& nums) {
 
 	// Minimum number of jumps we definitely have to take
 	int nj = 1;
-	// Invariant: numJumps[j] = minimum number of jumps
-	// to get nums[j] for 0 <= j < i
-	//
-	// Invariant: nj = minimum number of jumps to get to nums[i]
-	// If numJumps[j] <= nj for j < i
-	// Then num jumps to get to nums[k] for k <= j
-	// is <= numJumps[j]
 	for(int i = 1; i < nums.size(); ++i) {
-		assert(numJumps[i - 1] <= nj);
-		assert(nj <= numJumps[i]);
-		assert(numJumps[furthestMarker] == nj);
-		assert(nextFarMark == -1 || numJumps[nextFarMark] == nj + 1);
-
 		// furthest location we could jump from nums[i]
 		int maxJump = i + nums[i];
 
@@ -79,62 +60,16 @@ int jump(vector<int>& nums) {
 			return nj + 1;
 		}
 
-		// Mark upper end of range of numbers we can jump to from here
-		// Don't overwrite markers for shorter route
-		if(numJumps[maxJump] == INT_MAX) {
-			numJumps[maxJump] = nj + 1;
-			if(maxJump > nextFarMark) {
-				nextFarMark = maxJump;
-			}
-		}
+		nextFarMark = max(nextFarMark, maxJump);
 
 		// We found the marker for the end of a jump range
-		// Problem: there could be multiple markers
-		// Solution: keep track of farthest right marker
 		if(i == furthestMarker) {
 			// Looks like we have to take another jump
 			nj++;
 			furthestMarker = nextFarMark;
-			nextFarMark = -1;
-		} else {
-			// nums[i] is within range of on of the previous
-			// numbers
-			numJumps[i] = nj;
 		}
 	}
-	return numJumps.back();
-
-	// num jumps
-	// ------------------------------------------
-	// 0  inf inf inf inf inf inf nums[0] inf inf
-	// nj = 0
-	//
-	// 0  1 inf nums[1] inf inf inf nums[0] inf inf
-	// nj = 1
-	//
-	// 0  1 1 nums[1] inf nums[2] inf nums[0] inf inf
-	// nj = 1
-	//
-	// 0  1 1 1 inf nums[2] inf nums[0] inf nums[3]
-	// nj = 1
-	//
-	// start, range
-
-/*
-	[2,3,1,1,4]
-	2
-	0, <=1, <=1, ?, ?
-	2 3
-	0, 1, <=1, <=2, <=2
-
-	[1,2,100,1,1,1,1,1,1]
-	1
-	0, <=1, ?, ?, ?, ?, ?, ?, ? 
-	1 2
-	0, 1, <=2, <=2, ?, ?, ?, ?, ? 
-	1 2 100
-	0, 1, 2, <=2, <=3, <=3, <=3, <=3, <=3,
-*/
+	return nj;
 }
 
 struct TestCase {
